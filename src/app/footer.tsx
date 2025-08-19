ef1e83a8cad90e1d34c3e4fcbca812dea8f0cc43
linjy <jaysen.dev@gmail.com>
2025-08-19 23:33:02 +0800

    初始化代码仓库

---
+++ src/app/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="row-start-4 flex gap-[24px] flex-wrap items-center justify-center">
+      <p className="items-center">copyright 2025 林建有 all right reserved</p>
+    </footer>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,56 @@
 import Image from "next/image";
-
+import Footer from "./footer";
+function QRcodeLoginPanel() {
+  return (
+    <div>
+      <h2>通过二维码登录</h2>
+      <div className="flex flex-col items-center justify-center">
+      <Image id="qrcodeImg" src="/qrcode.png" alt="二维码" width={100} height={100} />
+        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+          <li className="tracking-[-.01em]">打开手机上的Telegram APP</li>
+          <li className="tracking-[-.01em]">
+            导航到设置&gt;设备…&gt;连接至桌面设备界面
+          </li>
+          <li className="tracking-[-.01em]">
+            在手机该界面上点击的扫描二维码按钮登录
+          </li>
+        </ol>
+        <button className="bg:dark round mt-6" type="button" id="switchPhoneNumber">使用手机号码登录</button>
+      </div>
+    </div>
+  );
+}
+function PhoneNumberLoginPanel() {
+  return (
+    <div>
+      <h2>通过手机号码登录</h2>
+      {/* 国家码 */}
+      <label htmlFor="countryCode">国家码：</label>
+      <input type="option" id="countryCode" className="input w-[80px]" defaultValue="+86"/>
+      {/* 手机号码输入框 */}
+      <br />
+      <label htmlFor="phoneNumberInput">手机号码：</label>
+      <div className="flex flex-col items-center justify-center">
+        <input
+          type="text"
+          id="phoneNumberInput"
+          className="input"
+          placeholder="请输入手机号码"
+        />
+        <button className="bg:dark round mt-6" type="button" id="sendCodeButton">下一步</button>
+      </div>
+    </div>
+  );
+}
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <QRcodeLoginPanel />
+        <PhoneNumberLoginPanel />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      {/* <Footer /> */}
     </div>
   );
 }
